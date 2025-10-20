e0fec0936295f67cac560fe6477bb167804607ef
Maxim Butenko <maxim.butenko@proton.me>
2025-10-20 19:38:37 +0300

    deployment test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://aszxqaz.github.io",
+  base: "/fm-loopstudios-landing-page",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
